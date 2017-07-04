ae7344e36dabf2b419cf55060d06ce3adc47bdeb
VitalyTen <vtenigin@student.42.us.org>
2017-07-04 15:46:47 -0700

    01

---
+++ d01/ex01/ex01.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vtenigin <vtenigin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/04 15:39:57 by vtenigin          #+#    #+#             */
+/*   Updated: 2017/07/04 15:46:28 by vtenigin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+void memoryLeak()
+{
+	std::string* panthere = new std::string("String panthere");
+	std::cout << *panthere << std::endl;
+	delete panthere;
+}
