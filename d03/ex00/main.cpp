67fdf081c9323c945ebe4510eb720486ee05c055
VitalyTen <vtenigin@student.42.us.org>
2017-07-06 18:48:17 -0700

    00

---
+++ d03/ex00/main.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vtenigin <vtenigin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/05 23:17:12 by vtenigin          #+#    #+#             */
+/*   Updated: 2017/07/06 18:46:05 by vtenigin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+int main(void) {
+	FragTrap ft0;
+	FragTrap ft1;
+	FragTrap ft2;
+	FragTrap ft3;
+	FragTrap ft4;
+	FragTrap ft5;
+	FragTrap ft6;
+	FragTrap ft7;
+	FragTrap ft8;
+	FragTrap ft9;
+	FragTrap ft10;
+	FragTrap ft11;
+	FragTrap ft12;
+	FragTrap ft13;
+	FragTrap ft14;
+	FragTrap ft15;
+	FragTrap ft16;
+	FragTrap ft17;
+	FragTrap ft18;
+	FragTrap ft19;
+	FragTrap ft20;
+	FragTrap ft21;
+	FragTrap ft22;
+	FragTrap ft23;
+	FragTrap ft24;
+	FragTrap ft25;
+	FragTrap ft26;
+	FragTrap ft27("ft27");
+	FragTrap ft28("ft28");
+	FragTrap ft29(ft26);
+	ft0 = ft27;
+	ft27.rangedAttack("tree");
+	ft27.meleeAttack("tree");
+	ft27.takeDamage(3);
+	ft27.takeDamage(30);
+	ft27.takeDamage(90);
+	ft27.beRepaired(50);
+	ft27.beRepaired(70);
+	ft27.vaulthunter_dot_exe("tree");
+	ft27.vaulthunter_dot_exe("tree");
+	ft27.vaulthunter_dot_exe("tree");
+	ft27.vaulthunter_dot_exe("tree");
+	ft27.vaulthunter_dot_exe("tree");
+	ft27.vaulthunter_dot_exe("tree");
+	return 0;
+}
